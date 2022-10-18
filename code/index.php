a902e6f81f4ff69f74ca070a1bd9d67c5ae2d4bd
sprokazin <subtir233@gmail.com>
2022-10-18 22:59:42 +0200

    10-16

--- code/index.php
+++ code/index.php
@@ -108,3 +108,214 @@ for ($i = 0; $i < 10; $i++) {
 echo '<pre>';
 print_r($randArr);
 echo '</pre>';
+
+//Одиннадцатое задание
+echo nl2br(PHP_EOL . "EleventhTask: " . PHP_EOL);
+$a = -312314;
+$b = 14241241;
+echo abs($a - $b);
+$absArr = [1, 2, -1, -2, 3, -3];
+for ($i = 0; $i < count($absArr); $i++) {
+    if ($absArr[$i] < 0)
+        $absArr[$i] = -1 * $absArr[$i];
+}
+echo '<pre>';
+print_r($absArr);
+echo '</pre>';
+
+//Двенадцатое задание
+echo nl2br(PHP_EOL . "TwelveTask: " . PHP_EOL);
+$num = 30;
+$delArr = [];
+for ($i = 1; $i <= ($num); $i++) {
+    if ($num % $i === 0)
+        $delArr[] = $i;
+}
+
+echo '<pre>';
+print_r($delArr);
+echo '</pre>';
+$sumArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$count = 0;
+$sum = 0;
+$i = 0;
+while ($sum < 10) {
+    $sum += $sumArr[$i];
+    $count++;
+    $i++;
+}
+echo nl2br(PHP_EOL . $count);
+
+//Тринадцатое задание
+echo nl2br(PHP_EOL . "ThirteenthTask: " . PHP_EOL);
+function printStringReturnNumber(string $i): int
+{
+    var_dump($i);
+    return (int)$i;
+}
+
+$newNumb = printStringReturnNumber('3');
+var_dump($newNumb);
+
+//Чертырнадцатое задание
+echo nl2br(PHP_EOL . "FourteenthTask: " . PHP_EOL);
+function increaseEnthusiasm(string $str): string
+{
+    return $str . '!';
+}
+
+echo nl2br(PHP_EOL . (increaseEnthusiasm('StringString')));
+
+function repeatThreeTimes(string $str): string
+{
+	return str_repeat($str, 3);
+}
+
+echo nl2br(PHP_EOL . (repeatThreeTimes('STR')));
+echo nl2br(PHP_EOL . (increaseEnthusiasm(repeatThreeTimes('STR'))));
+
+function cut(string $str, int $limit = 10) {}
+
+function printArrayRecursive(array $arr, $i = 0): void
+{
+	if ($i >= count($arr))
+	{
+		return;
+	}
+	echo $arr[$i] . ' ';
+	printArrayRecursive($arr, $i + 1);
+}
+
+printArrayRecursive($arr);
+
+function getDigitsSum(int $number): int
+{
+	$sum = 0;
+	while ($number > 0)
+	{
+		$sum += $number % 10;
+		$number /= 10;
+	}
+	return $sum;
+}
+
+$number = 1421;
+$result = getDigitsSum($number);
+while ($result >= 10)
+{
+	$result = getDigitsSum($result);
+}
+echo nl2br(PHP_EOL . $result . PHP_EOL);
+
+//Пятнадцатое задание
+echo nl2br(PHP_EOL . "SixteenthTask: " . PHP_EOL);
+function repeatSymbol(string $symbol, int $count): array
+{
+    $arr = [];
+    for ($i = 1; $i <= $count; $i++) {
+        $arr[$i] = str_repeat($symbol, $i);
+    }
+    return $arr;
+}
+
+$ansArr = repeatSymbol('x', 10);
+echo '<pre>';
+print_r($ansArr);
+echo '</pre>';
+function arrayFill(string $symbol, int $count): array
+{
+    $arr = [];
+    for ($i = 1; $i <= $count; $i++) {
+        $arr[$i] = $symbol;
+    }
+    return $arr;
+}
+
+$ansArr = arrayFill('x', 5);
+echo '<pre>';
+print_r($ansArr);
+echo '</pre>';
+$arr = [[]];
+$arr = [[1, 2, 3], [4, 5], [6]];
+$sum = 0;
+foreach ($arr as $item) {
+    foreach ($item as $value) {
+        $sum += $value;
+    }
+}
+echo $sum;
+$arrS = [[]];
+$c = 1;
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        $arrS[$i][$j] = $c;
+        $c++;
+    }
+}
+echo '<pre>';
+print_r($arrS);
+echo '</pre>';
+$arr = [2, 5, 3, 9];
+$result = $arr[0] * $arr[1] + $arr[2] * $arr[3];
+echo nl2br(PHP_EOL . $result . PHP_EOL);
+$user = ['name' => 'Sergey', 'surname' => 'Prokazin', 'patronymic' => 'Aleksandrovich'];
+
+echo nl2br(PHP_EOL . $user['name'] . ' ' . $user['surname'] . ' ' . $user['patronymic'] . PHP_EOL);
+
+$date = ['year' => 2022, 'month' => 'September', 'day' => 18];
+echo nl2br(PHP_EOL . $date['year'] . '-' . $date['month'] . '-' . $date['day'] . PHP_EOL);
+$arr = ['a', 'b', 'c', 'd', 'e'];
+echo nl2br(PHP_EOL . count($arr) . PHP_EOL);
+echo nl2br(PHP_EOL . $arr[count($arr) - 1] . ' ' . $arr[count($arr) - 2] . PHP_EOL);
+
+//Шестнадцатое задание
+echo nl2br(PHP_EOL . "SeventeenthTask: " . PHP_EOL);
+function getSumBoolRes(int $num1, int $num2): bool
+{
+    if ($num1 + $num2 > 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$flag = getSumBoolRes(7, 0);
+var_dump($flag);
+function getEqualsBoolRes(int $num1, int $num2): bool
+{
+    if ($num1 === $num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$flag = getEqualsBoolRes(0, 0);
+var_dump($flag);
+$test = 12;
+echo $test == 0 ? 'Верно' : 'Не верно';
+$age = 14;
+if ($age < 10 || $age > 99) {
+    echo nl2br(PHP_EOL . 'Возраст меньше 10 или больше 99');
+} else {
+    $ret = [];
+    $sum = 0;
+    for ($i = 0; $age > 9; $i++) {
+        $ret[$i] = $age % 10;
+        $age = $age / 10;
+        $sum = $sum + $ret[$i];
+    }
+    if ($sum <= 9) {
+        echo nl2br(PHP_EOL . 'Сумма цифр однозначна');
+    } else {
+        echo nl2br(PHP_EOL . 'Сумма цифр двузначна');
+    }
+}
+$arr = [1, 2, 3];
+$sumArr = 0;
+if (count($arr) === 3) {
+    for ($i = 0; $i < count($arr); $i++) {
+        $sumArr += $arr[$i];
+    }
+    echo nl2br(PHP_EOL . $sumArr);
+}
